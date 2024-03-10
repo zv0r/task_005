5928d94828f0abad9647bc863e56615ed5976e69
Olga Komleva <olyakomleva@mail.ru>
2024-03-10 21:07:04 +0300

    half of tests morse

---
+++ tests/test_morse.py
@@ -0,0 +1,98 @@
+from subprocess import run, PIPE
+
+import os
+import pytest
+
+B_FILE_PATH = './bin/morse'
+
+
+def run_test_with_error_input(input_str):
+    result = run(
+        [B_FILE_PATH], input=input_str, encoding='utf-8',
+        stdout=PIPE, stderr=PIPE
+    )
+    assert result.returncode != 0
+    assert "Puck you, Verter!" in result.stderr
+
+
+def run_test_with_correct_input(input_str, expected_stdout):
+    result = run([B_FILE_PATH], input=input_str, encoding='utf-8', stdout=PIPE)
+    assert result.returncode != 0
+    assert result.stdout == expected_stdout
+
+
+if os.path.isfile(B_FILE_PATH):
+    def test_bin_folder_containts_morse():
+        assert os.path.isfile(B_FILE_PATH)
+
+    def test_morse_incorrect_command_input_1():
+        run_test_with_error_input('0\nSOS')
+
+    def test_morse_incorrect_command_input_2():
+        run_test_with_error_input('9\nSOS')
+
+    def test_morse_incorrect_command_input_3():
+        run_test_with_error_input('777\nSOS')
+
+    def test_morse_incorrect_command_input_4():
+        run_test_with_error_input('0\nSOS')
+
+    def test_morse_incorrect_command_input_5():
+        run_test_with_error_input('-5\nSOS')
+
+    def test_morse_incorrect_value_input_1():
+        run_test_with_error_input('1\nПРИВЕТ')
+
+    def test_morse_incorrect_value_input_2():
+        run_test_with_error_input('1\nSOS!')
+
+    def test_morse_incorrect_value_input_3():
+        run_test_with_error_input('1\n... --- ...')
+
+    def test_morse_incorrect_value_input_4():
+        run_test_with_error_input('2\nSOS')
+
+    def test_morse_incorrect_value_input_5():
+        run_test_with_error_input('1\nSos -3')
+
+    def test_letters_into_morse_1():
+        run_test_with_correct_input('1\nSOS', '... --- ...')
+
+    def test_letters_into_morse_2():
+        run_test_with_correct_input('1\n15', '.---- .....')
+
+    def test_letters_into_morse_3():
+        run_test_with_correct_input('1\n15S', '.---- ..... ...')
+
+    def test_letters_into_morse_4():
+        input_str = '1\nhello'
+        exp_output = '.... . .-.. .-.. ---'
+        run_test_with_correct_input(input_str, exp_output)
+
+    def test_letters_into_morse_5():
+        input_str = '1\nHELLO'
+        exp_output = '.... . .-.. .-.. ---'
+        run_test_with_correct_input(input_str, exp_output)
+
+    def test_letters_into_morse_6():
+        input_str = '1\nHeLlO'
+        exp_output = '.... . .-.. .-.. ---'
+        run_test_with_correct_input(input_str, exp_output)
+    
+    def test_words_into_morse_1():
+        input_str = '1\nHello world'
+        exp_output = '.... . .-.. .-.. ---\t.-- --- .-. .-.. -..'
+        run_test_with_correct_input(input_str, exp_output)
+    
+    def test_words_into_morse_2():
+        input_str = '1\nWe can do it'
+        exp_output = '.-- .\t-.-. .- -.\t-.. ---\t.. -'
+        run_test_with_correct_input(input_str, exp_output)
+    
+    def test_words_into_morse_():
+        input_str = '1\n20 000 Leagues Under The Sea'
+        exp_output = '..--- '
+        run_test_with_correct_input(input_str, exp_output)
+
+if __name__ == '__main__':
+    pytest.main()
